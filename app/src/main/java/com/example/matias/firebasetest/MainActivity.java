17a58977208b7e00dec9acef49e0009af69664c9
tutee <tute.guazzaroni@gmail.com>
2016-04-28 14:54:42 -0300

    Inicio del branch

--- app/src/main/java/com/example/matias/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/matias/firebasetest/MainActivity.java
@@ -17,15 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+        //asd
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
